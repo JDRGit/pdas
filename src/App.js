600430374e43318e064195bcbbdd85e3870b7e72
Jaime D. Rodriguez <118196254+JDRGit@users.noreply.github.com>
2023-03-02 16:22:45 -0500

    added description to app

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ import SiriImage from "./images/siri.png";
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
 
       <div className="container">
         <section className="section">
           <div className="columns">
             <div className="column is-4">
-              <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+              <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} 
+              description="Alexa is a virtual assistant created by Amazon. It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news. Alexa can also control several smart devices using itself as a home automation system."
+              />
             </div>
 
             <div className="column is-4">
@@ -21,11 +27,14 @@ function App() {
                 title="Cortana"
                 handle="@cortana32"
                 image={CortanaImage}
+                description="Cortana is a virtual assistant created by Microsoft for Windows 10, Windows 10 Mobile, Windows Phone 8.1, Invoke smart speakers, Microsoft Band, Xbox One, iOS, Android, and Windows Mixed Reality. Cortana is capable of voice interaction, making to-do lists, setting reminders, playing music, answering questions, and providing local search results. Cortana can also control several smart devices using itself as a home automation system."
               />
             </div>
 
             <div className="column is-4">
-              <ProfileCard title="Siri" handle="siri01" image={SiriImage} />
+              <ProfileCard title="Siri" handle="siri01" image={SiriImage}
+              description="Siri is a virtual assistant developed by Apple Inc. that is available on Apple devices. Siri can understand voice commands and complete tasks such as making phone calls, sending text messages, setting reminders, and playing music. Siri can also control several smart devices using itself as a home automation system."
+              />
             </div>
           </div>
         </section>
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,8 +1,8 @@
-function ProfileCard({ title, handle, image }) {
+function ProfileCard({ title, handle, image, description }) {
   return (
     <div className="card">
         <div className="card-image" >
-            <figure className="image is-4by3">
+            <figure className="image is-1by1">
                 <img src={image} alt="pda logo" />
             </figure>
         </div>
@@ -12,6 +12,7 @@ function ProfileCard({ title, handle, image }) {
                 <p className="title is-4">{title}</p>
                 <p className="subtitle is-6">{handle}</p>
             </div>
+            <div className="content">{description}</div> 
         </div>
     </div>
   );
